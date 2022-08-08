af12acc4addb1e6fbaba0da39d9c34686d51e508
chiachun wu <a0283137@gmail.com>
2022-08-08 22:40:24 +0800

    增加留白

--- currency.php
+++ currency.php
@@ -20,182 +20,210 @@ foreach($currency as $key=>$value){
             // echo     $country[1]."<br>";      //國名
 
 
-            echo '<div class="card col-8 col-sm-6 col-md-3" >';
+            echo '<div class=" col-8 col-sm-6 col-md-3 my-3">';
+            echo '<div class="card">';
             echo '<img class="card-img-top" src=./img/'.$country[1].'.png alt="Card image" style="width:100%">';
             echo '<div class="card-body">';
             echo '<h4 class="card-title">USD=>'.$country[1].'</h4>';
             echo '<p class="card-text">'.$value->Exrate.'</p>';
             echo '<p class="card-text" style="color:rgb(255, 59, 160);">Update at UTC</p>';
             echo '<p class="card-text">'.$value->UTC.'</p>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
             break;
 
 
         case 'USDCAD':
-            echo '<div class="card col-8 col-sm-6 col-md-3" >';
+            echo '<div class=" col-8 col-sm-6 col-md-3 my-3">';
+            echo '<div class="card">';
             echo '<img class="card-img-top" src=./img/'.$country[1].'.png alt="Card image" style="width:100%">';
             echo '<div class="card-body">';
             echo '<h4 class="card-title">USD=>'.$country[1].'</h4>';
             echo '<p class="card-text">'.$value->Exrate.'</p>';
             echo '<p class="card-text" style="color:rgb(255, 59, 160);">Update at UTC</p>';
             echo '<p class="card-text">'.$value->UTC.'</p>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
             break;
 
 
         case 'USDEUR':
-            echo '<div class="card col-8 col-sm-6 col-md-3" >';
+            echo '<div class=" col-8 col-sm-6 col-md-3 my-3">';
+            echo '<div class="card">';
             echo '<img class="card-img-top" src=./img/'.$country[1].'.png alt="Card image" style="width:100%">';
             echo '<div class="card-body">';
             echo '<h4 class="card-title">USD=>'.$country[1].'</h4>';
             echo '<p class="card-text">'.$value->Exrate.'</p>';
             echo '<p class="card-text" style="color:rgb(255, 59, 160);">Update at UTC</p>';
             echo '<p class="card-text">'.$value->UTC.'</p>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
             break;
 
 
         case 'USDGBP':
-            echo '<div class="card col-8 col-sm-6 col-md-3" >';
+            echo '<div class=" col-8 col-sm-6 col-md-3 my-3">';
+            echo '<div class="card">';
             echo '<img class="card-img-top" src=./img/'.$country[1].'.png alt="Card image" style="width:100%">';
             echo '<div class="card-body">';
             echo '<h4 class="card-title">USD=>'.$country[1].'</h4>';
             echo '<p class="card-text">'.$value->Exrate.'</p>';
             echo '<p class="card-text" style="color:rgb(255, 59, 160);">Update at UTC</p>';
             echo '<p class="card-text">'.$value->UTC.'</p>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
             break;
 
 
         case 'USDHKD':
-            echo '<div class="card col-8 col-sm-6 col-md-3" >';
+            echo '<div class=" col-8 col-sm-6 col-md-3 my-3">';
+            echo '<div class="card">';
             echo '<img class="card-img-top" src=./img/'.$country[1].'.png alt="Card image" style="width:100%">';
             echo '<div class="card-body">';
             echo '<h4 class="card-title">USD=>'.$country[1].'</h4>';
             echo '<p class="card-text">'.$value->Exrate.'</p>';
             echo '<p class="card-text" style="color:rgb(255, 59, 160);">Update at UTC</p>';
             echo '<p class="card-text">'.$value->UTC.'</p>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
             break;
 
 
         case 'USDINR':
-            echo '<div class="card col-8 col-sm-6 col-md-3" >';
+            echo '<div class=" col-8 col-sm-6 col-md-3 my-3">';
+            echo '<div class="card">';
             echo '<img class="card-img-top" src=./img/'.$country[1].'.png alt="Card image" style="width:100%">';
             echo '<div class="card-body">';
             echo '<h4 class="card-title">USD=>'.$country[1].'</h4>';
             echo '<p class="card-text">'.$value->Exrate.'</p>';
             echo '<p class="card-text" style="color:rgb(255, 59, 160);">Update at UTC</p>';
             echo '<p class="card-text">'.$value->UTC.'</p>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
             break;
 
 
         case 'USDJPY':
-            echo '<div class="card col-8 col-sm-6 col-md-3" >';
+            echo '<div class=" col-8 col-sm-6 col-md-3 my-3">';
+            echo '<div class="card">';
             echo '<img class="card-img-top" src=./img/'.$country[1].'.png alt="Card image" style="width:100%">';
             echo '<div class="card-body">';
             echo '<h4 class="card-title">USD=>'.$country[1].'</h4>';
             echo '<p class="card-text">'.$value->Exrate.'</p>';
             echo '<p class="card-text" style="color:rgb(255, 59, 160);">Update at UTC</p>';
             echo '<p class="card-text">'.$value->UTC.'</p>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
             break;
 
 
         case 'USDKRW':
-            echo '<div class="card col-8 col-sm-6 col-md-3" >';
+            echo '<div class=" col-8 col-sm-6 col-md-3 my-3">';
+            echo '<div class="card">';
             echo '<img class="card-img-top" src=./img/'.$country[1].'.png alt="Card image" style="width:100%">';
             echo '<div class="card-body">';
             echo '<h4 class="card-title">USD=>'.$country[1].'</h4>';
             echo '<p class="card-text">'.$value->Exrate.'</p>';
             echo '<p class="card-text" style="color:rgb(255, 59, 160);">Update at UTC</p>';
             echo '<p class="card-text">'.$value->UTC.'</p>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
             break;
 
 
         case 'USDNZD':
-            echo '<div class="card col-8 col-sm-6 col-md-3" >';
+            echo '<div class=" col-8 col-sm-6 col-md-3 my-3">';
+            echo '<div class="card">';
             echo '<img class="card-img-top" src=./img/'.$country[1].'.png alt="Card image" style="width:100%">';
             echo '<div class="card-body">';
             echo '<h4 class="card-title">USD=>'.$country[1].'</h4>';
             echo '<p class="card-text">'.$value->Exrate.'</p>';
             echo '<p class="card-text" style="color:rgb(255, 59, 160);">Update at UTC</p>';
             echo '<p class="card-text">'.$value->UTC.'</p>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
             break;
 
 
         case 'USDRUB':
-            echo '<div class="card col-8 col-sm-6 col-md-3" >';
+            echo '<div class=" col-8 col-sm-6 col-md-3 my-3">';
+            echo '<div class="card">';
             echo '<img class="card-img-top" src=./img/'.$country[1].'.png alt="Card image" style="width:100%">';
             echo '<div class="card-body">';
             echo '<h4 class="card-title">USD=>'.$country[1].'</h4>';
             echo '<p class="card-text">'.$value->Exrate.'</p>';
             echo '<p class="card-text" style="color:rgb(255, 59, 160);">Update at UTC</p>';
             echo '<p class="card-text">'.$value->UTC.'</p>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
             break;
 
 
         case 'USDSGD':
-            echo '<div class="card col-8 col-sm-6 col-md-3" >';
+            echo '<div class=" col-8 col-sm-6 col-md-3 my-3">';
+            echo '<div class="card">';
             echo '<img class="card-img-top" src=./img/'.$country[1].'.png alt="Card image" style="width:100%">';
             echo '<div class="card-body">';
             echo '<h4 class="card-title">USD=>'.$country[1].'</h4>';
             echo '<p class="card-text">'.$value->Exrate.'</p>';
             echo '<p class="card-text" style="color:rgb(255, 59, 160);">Update at UTC</p>';
             echo '<p class="card-text">'.$value->UTC.'</p>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
             break;
 
 
         case 'USDTHB':
-            echo '<div class="card col-8 col-sm-6 col-md-3" >';
+            echo '<div class=" col-8 col-sm-6 col-md-3 my-3">';
+            echo '<div class="card">';
             echo '<img class="card-img-top" src=./img/'.$country[1].'.png alt="Card image" style="width:100%">';
             echo '<div class="card-body">';
             echo '<h4 class="card-title">USD=>'.$country[1].'</h4>';
             echo '<p class="card-text">'.$value->Exrate.'</p>';
             echo '<p class="card-text" style="color:rgb(255, 59, 160);">Update at UTC</p>';
             echo '<p class="card-text">'.$value->UTC.'</p>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
             break;
 
 
         case 'USDTWD':
-            echo '<div class="card col-8 col-sm-6 col-md-3" >';
+            echo '<div class=" col-8 col-sm-6 col-md-3 my-3">';
+            echo '<div class="card">';
             echo '<img class="card-img-top" src=./img/'.$country[1].'.png alt="Card image" style="width:100%">';
             echo '<div class="card-body">';
             echo '<h4 class="card-title">USD=>'.$country[1].'</h4>';
             echo '<p class="card-text">'.$value->Exrate.'</p>';
             echo '<p class="card-text" style="color:rgb(255, 59, 160);">Update at UTC</p>';
             echo '<p class="card-text">'.$value->UTC.'</p>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
             break;
 
 
         case 'USDZAR':
-            echo '<div class="card col-8 col-sm-6 col-md-3" >';
+            echo '<div class=" col-8 col-sm-6 col-md-3 my-3">';
+            echo '<div class="card">';
             echo '<img class="card-img-top" src=./img/'.$country[1].'.png alt="Card image" style="width:100%">';
             echo '<div class="card-body">';
             echo '<h4 class="card-title">USD=>'.$country[1].'</h4>';
             echo '<p class="card-text">'.$value->Exrate.'</p>';
             echo '<p class="card-text" style="color:rgb(255, 59, 160);">Update at UTC</p>';
             echo '<p class="card-text">'.$value->UTC.'</p>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
             break;
